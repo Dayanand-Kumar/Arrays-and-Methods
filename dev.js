e8856154c268a42c24ae59a78df6a2226e98e396
Dayanand-Kumar <angularexpertdayanand@gmail.com>
2021-04-13 21:31:45 +0530

    map and interview questions added

--- dev.js
+++ dev.js
@@ -1,7 +1,15 @@
 const product = {
-    productId : '101',
-    name : 'Soap',
-    price : '50 INR'
+    productId: '101',
+    name: 'Soap',
+    price: '50 INR'
 }
 
 console.log(product);
+
+const products = ['Detergent', 'oil', 'Rice', 'Tea powder'];
+for (let product of products) {
+    console.log("Item is", product);
+}
+
+//Map
+console.log(products.map(product => 'item : '+product));
